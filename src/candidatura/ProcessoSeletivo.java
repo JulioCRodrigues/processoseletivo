55ef47b6f4ba1033be126cebbffc5e336dcc5f46
JULIO CESAR RODRIGUES <jurodrigues@brasilseg.com.br>
2024-09-03 21:53:18 -0300

    Criando método para analisar candidato

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -2,6 +2,21 @@ package candidatura;
 
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
+		System.out.println("=== PROCESSO SELETIVO ===");
+	}
+	
+	static void analisarCandidato(double salarioPretendido) {
+		
+		double salarioBase = 2000.0;
+		
+		if(salarioBase > salarioPretendido) {
+			System.out.println("Ligar para o candidato...");
+		} else if(salarioBase == salarioPretendido) {
+			System.out.println("Ligar para o candidato com contra proposta");
+			
+		} else {
+			System.out.println("Aguardando o resultado dos demais candidatos...");
+		}
 		
 	}
 
