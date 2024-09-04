553ecac573cec65e1cdeb69b83cd0488ebfb9143
JULIO CESAR RODRIGUES <jurodrigues@brasilseg.com.br>
2024-09-03 22:28:34 -0300

    Selecionando candidatos

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,8 +1,36 @@
 package candidatura;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
 		System.out.println("=== PROCESSO SELETIVO ===");
+		
+		
+	}
+	
+	static void selecaoCandidatos() {
+		String [] candidatos = {"MARCOS", "PAULO", "MARTA", "MARIA", "MARCELA", "HENRIQUE", "LUCAS", "GABRIEL","ADRIANA", "MONICA"};
+		
+		int candidatosSelecionados = 0;
+		int candidatoAtual = 0;
+		double salarioBase = 2000.0;
+		
+		while(candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+			String candidato = candidatos[candidatoAtual];
+			double salarioPretendido = valorPretendido();
+			
+			System.out.println("O candidato " + candidato + " solicitou o salario " + salarioPretendido);
+			
+			if(salarioBase >= salarioPretendido) {
+				System.out.println("O candidato " +  candidato + "foi selecionado para a vaga!");
+				candidatosSelecionados++;
+			}
+			candidatoAtual++;
+		}
+	}
+	
+	static double valorPretendido() {
+		return ThreadLocalRandom.current().nextDouble(1800, 2200);
 	}
 	
 	static void analisarCandidato(double salarioPretendido) {
@@ -19,5 +47,8 @@ public class ProcessoSeletivo {
 		}
 		
 	}
+	
+
+	
 
 }
